4df73b877e8c058c4b3631face6f48c46a67aafd
Marvish <mschandr@uci.edu>
2022-08-04 08:49:05 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Marvish Chandra
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import Math
+
+
+def gatewayShape(A,B,C,h,x):
+    A = .69
+    catenary = 1 / A # catenary being the calculated B constant
+    y = A * Math.cos * h(catenary * x) + C
